a1b529cba3dcbe2b48b876bfdd1b8cdb0b2484e4
Ryan Canavan <rycanavan@gmail.com>
2017-09-16 22:08:14 -0400

    Common tracks filter

--- client/src/components/TrackList.js
+++ client/src/components/TrackList.js
@@ -1,16 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Track from './Track';
 
-function TrackList(props) {
-	return (
-		<div className="TrackList">
-			<h2>{props.playlistName}</h2>
-			<button className="ResetButton" onClick={props.onClick}><b>Choose Different Playlist</b></button>
-			{props.data.map((track, index) =>
-				<Track key={index} data={track} commonTracks={props.commonTracks} />
-			)}
-		</div>
-	)
+class TrackList extends Component {
+	constructor(){
+		super();
+		this.state = {
+			sort: "oldest",
+			filter: false,
+		};
+	}
+
+	filterChange() {
+		const newFilter = this.state.filter ? false : true;
+		this.setState({
+			filter: newFilter
+		});
+	}
+
+	render() {
+		var tracks = this.props.data;
+		if(this.props.commonTracks.length !== 0 && this.state.filter){
+			tracks = this.props.commonTracks;
+		}
+		return (
+			<div className="TrackList">
+				<button className="ResetButton" onClick={this.props.onClick}><b>Choose Different Playlist</b></button>
+				<h2>{this.props.playlistName}</h2>
+				<div className="Sort">
+					<form>
+						<label>Sort By: </label>
+						<select defaultValue={this.state.sort} onChange={this.orderChange}>
+							<option value="oldest">Oldest First</option>
+							<option value="newest">Newest First</option>
+							<option value="alphabetical">A-Z</option>
+						</select>
+					</form>
+				</div>
+				<div className="Filter">
+					<form>
+						<label style={{float: "left"}}>Filter : </label>
+						<label className="switch">
+							<input type="checkbox" checked={this.state.filter} onChange={this.filterChange.bind(this)} />
+							<span className="slider round"></span>
+						</label>
+					</form>
+				</div>
+				{tracks.map((track, index) =>
+					<Track key={index} data={track} commonTracks={this.props.commonTracks} />
+				)}
+			</div>
+		)
+	}
 }
 
 export default TrackList;
