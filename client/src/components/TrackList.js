99b6685a7ca156c75b7f8cdcff67d0644199a5b7
ryancanavan <rycanavan@gmail.com>
2017-09-16 23:38:38 -0400

    Added sorting

--- client/src/components/TrackList.js
+++ client/src/components/TrackList.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import Track from './Track';
 
 class TrackList extends Component {
-	constructor(){
-		super();
+	constructor(props){
+		super(props);
 		this.state = {
 			sort: "oldest",
 			filter: false,
+			playlist: props.data,
 		};
+	}
+
+	componentWillReceiveProps(nextProps) {
+		this.setState({
+			playlist: nextProps.data
+		})
 	}
 
 	filterChange() {
@@ -17,8 +24,65 @@ class TrackList extends Component {
 		});
 	}
 
+	orderChange(event) {
+		switch(event.target.value) {
+			case "oldest":
+				let oldestPlaylist = this.state.playlist.sort(function(a, b){
+					var dateA = new Date(a.added_at);
+					var dateB = new Date(b.added_at);
+					return dateA - dateB;
+				});
+				this.setState({
+					playlist: oldestPlaylist
+				});
+				break;
+			case "newest":
+				let newestPlaylist = this.state.playlist.sort(function(a, b){
+					var dateA = new Date(a.added_at);
+					var dateB = new Date(b.added_at);
+					return dateA - dateB;
+				});
+				newestPlaylist.reverse();
+				this.setState({
+					playlist: newestPlaylist
+				});
+				break;
+			case "alphabetical":
+				let alphabeticalPlaylist = this.state.playlist.sort(function(a, b){
+					var nameA = a.track.name.toUpperCase();
+					var nameB = b.track.name.toUpperCase();
+					if(nameA < nameB)
+						return -1;
+					if(nameB < nameA)
+						return 1;
+					return 0;
+				});
+				this.setState({
+					playlist: alphabeticalPlaylist
+				});
+				break;
+			case "reverseAlphabetical":
+				let reverseAlphabeticalPlaylist = this.state.playlist.sort(function(a, b){
+					var nameA = a.track.name.toUpperCase();
+					var nameB = b.track.name.toUpperCase();
+					if(nameA < nameB)
+						return -1;
+					if(nameB < nameA)
+						return 1;
+					return 0;
+				});
+				reverseAlphabeticalPlaylist.reverse();
+				this.setState({
+					playlist: reverseAlphabeticalPlaylist
+				});
+				break;
+			default:
+				break;
+		}
+	}
+
 	render() {
-		var tracks = this.props.data;
+		var tracks = this.state.playlist;
 		if(this.props.commonTracks.length !== 0 && this.state.filter){
 			tracks = this.props.commonTracks;
 		}
@@ -29,10 +93,11 @@ class TrackList extends Component {
 				<div className="Sort">
 					<form>
 						<label>Sort By: </label>
-						<select defaultValue={this.state.sort} onChange={this.orderChange}>
+						<select defaultValue={this.state.sort} onChange={this.orderChange.bind(this)}>
 							<option value="oldest">Oldest First</option>
 							<option value="newest">Newest First</option>
 							<option value="alphabetical">A-Z</option>
+							<option value="reverseAlphabetical">Z-A</option>
 						</select>
 					</form>
 				</div>
